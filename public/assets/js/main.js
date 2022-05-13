8e6236fcdca0db8ebc71a0b7f4a0656e212091b0
RnLamber <rnlamber@uci.edu>
2022-05-13 15:51:59 -0400

    test

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -36,7 +36,7 @@ socket.on('join_room_response',(payload)=> {
     }
     let newString = '<p class=\'join_room_response\'>'+payload.username+' joined the '+payload.room+'. (There are '+payload.count+' users in this room)</p>';
     $('#messages').prepend(newString);
-});
+})
 
 /*request to join the chatroom */
 
