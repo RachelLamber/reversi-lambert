40fd864fe7faac6df533b06e3cc087b4861fa07c
RnLamber <rnlamber@uci.edu>
2022-05-19 12:46:51 -0400

    more UI changes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -39,8 +39,11 @@ socket.on('join_room_response',(payload)=> {
         console.log(payload.message);
         return;
     }
-    let newString = '<p class=\'join_room_response\'>'+payload.username+' joined the '+payload.room+'. (There are '+payload.count+' users in this room)</p>';
-    $('#messages').prepend(newString);
+    let newHTML = '<p class=\'join_room_response\'>'+payload.username+' joined the '+payload.room+'. (There are '+payload.count+' users in this room)</p>';
+    let newNode = $(newHTML);
+    newNode.hide();
+    $('#messages').prepend(newNode);
+    newNode.show("fade",500);
 })
 
 function sendChatMessage(){
@@ -50,6 +53,7 @@ function sendChatMessage(){
     request.message = $('#chatMessage').val();
     console.log('**** Client log message, sending \'send_chat_message\' command: '+JSON.stringify(request));
     socket.emit('send_chat_message',request);
+    $('#chatMessage').val("");
 }
 socket.on('send_chat_message_response',(payload)=> {
     if((typeof payload == 'undefined') || (payload === null)){
@@ -60,8 +64,11 @@ socket.on('send_chat_message_response',(payload)=> {
         console.log(payload.message);
         return;
     }
-    let newString = '<p class=\'chat_message\'><b>'+payload.username+'</b>: '+payload.message+'</p>';
-    $('#messages').prepend(newString);
+    let newHTML = '<p class=\'chat_message\'><b>'+payload.username+'</b>: '+payload.message+'</p>';
+    let newNode = $(newHTML);
+    newNode.hide();
+    $('#messages').prepend(newNode);
+    newNode.show("fade",500);
 })
 
 /*request to join the chatroom */
