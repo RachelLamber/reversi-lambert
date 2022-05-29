f7188f77605c9d92054df96b31cd36ceaed1ce06
RnLamber <rnlamber@uci.edu>
2022-05-29 13:12:46 -0400

    debug

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -323,7 +323,7 @@ socket.on('game_update',(payload)=> {
             }
 
             const t = Date.now();
-            $('#'+row+'_'+column).html('<img class="img-fluid" src="assets/images/'+graphic+'?time='+t+'" alt="'+altTag+'" />');
+            $('#'+row+'_'+column).html('<img class="img-fluid" src="assets\'images\''+graphic+'?time='+t+'" alt="'+altTag+'" />');
         }
         }
     }
